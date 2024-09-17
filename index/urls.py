cd0a2574178ac93bd5672ff62ca411bea37b0220
Mikhail Mikhailusov <mishael000@yandex.ru>
2024-09-17 21:08:11 +0300

    update template

--- index/urls.py
+++ index/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from index.apps import IndexConfig
-from index.views import index
+from index.views import index, catalog
 
 app_name = IndexConfig.name
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('catalog/', catalog, name='catalog'),
 ]
--- index/views.py
+++ index/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index/index.html')
+
+
+def catalog(request):
+    return render(request, 'index/catalog.html')
