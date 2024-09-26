756101294ae1121abba3cd14c96695eb6820ca8c
Mikhail Mikhailusov <mishael000@yandex.ru>
2024-09-27 00:29:42 +0300

    .

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
 from index.apps import IndexConfig
-from index.views import index_view, catalog_view, furniture_leisure_view, furniture_kitchen_view, \
-    furniture_child_room_view, furniture_bathroom_view, furniture_for_work_view
+from index.views import index_view, catalog_view, products_by_category_view
 
 app_name = IndexConfig.name
 
@@ -10,9 +9,9 @@ urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/img/favicon.ico'), name='favicon'),
     path('', index_view, name='index'),
     path('catalog/', catalog_view, name='catalog'),
-    path('furniture_leisure/', furniture_leisure_view, name='furniture_leisure'),
-    path('furniture_for_work/', furniture_for_work_view, name='furniture_for_work'),
-    path('furniture_kitchen/', furniture_kitchen_view, name='furniture_kitchen'),
-    path('furniture_child_room/', furniture_child_room_view, name='furniture_child_room'),
-    path('furniture_bathroom/', furniture_bathroom_view, name='furniture_bathroom'),
+    path('furniture_leisure/', products_by_category_view, {'category_name': 'Мебель для отдыха', 'page_title': 'Мебель для отдыха', 'header_class': 'top-leisure'}, name='furniture_leisure'),
+    path('furniture_for_work/', products_by_category_view, {'category_name': 'Мебель для работы', 'page_title': 'Мебель для работы', 'header_class': 'top-work'}, name='furniture_for_work'),
+    path('furniture_kitchen/', products_by_category_view, {'category_name': 'Мебель для кухни', 'page_title': 'Мебель для кухни', 'header_class': 'top-kitchen'}, name='furniture_kitchen'),
+    path('furniture_child_room/', products_by_category_view, {'category_name': 'Мебель для детской', 'page_title': 'Мебель для детской', 'header_class': 'top-child-room'}, name='furniture_child_room'),
+    path('furniture_bathroom/', products_by_category_view, {'category_name': 'Мебель для ванной', 'page_title': 'Мебель для ванной', 'header_class': 'top-bathroom'}, name='furniture_bathroom'),
 ]
--- index/views.py
+++ index/views.py
@@ -8,40 +8,96 @@ def index_view(request):
     return render(
         request,
         'index/index.html',
-        {'special_offers': special_offers, 'latest_products': latest_products})
+        {
+            'special_offers': special_offers,
+            'latest_products': latest_products,
+            'header_class': 'top',
+        }
+    )
 
 
 def catalog_view(request):
     products = Product.objects.exclude(specialoffers__isnull=False).all()
-    return render(request, 'index/catalog.html', {'products': products})
-
-
-def furniture_leisure_view(request):
-    furniture_leisure = Product.objects.filter(category__name="Мебель для отдыха").exclude(specialoffers__isnull=False)
-    return render(request, 'index/furniture_leisure.html', {'products': furniture_leisure})
-
-
-def furniture_for_work_view(request):
-    furniture_for_work = Product.objects.filter(category__name="Мебель для работы").exclude(specialoffers__isnull=False)
-    return render(request, 'index/furniture_for_work.html', {'products': furniture_for_work})
-
-
-def furniture_kitchen_view(request):
-    furniture_kitchen = Product.objects.filter(category__name="Мебель для кухни").exclude(specialoffers__isnull=False)
-    return render(request, 'index/furniture_kitchen.html', {'products': furniture_kitchen})
-
-
-def furniture_child_room_view(request):
-    furniture_child_room = (
-        Product.objects.filter(category__name="Мебель для детской").exclude(specialoffers__isnull=False)
-    )
     return render(
         request,
-        'index/furniture_child_room.html',
-        {'products': furniture_child_room}
+        'index/catalog.html',
+        {
+            'products': products,
+            'header_class': 'top_catalog',
+        }
     )
 
 
-def furniture_bathroom_view(request):
-    furniture_bathroom = Product.objects.filter(category__name="Мебель для ванной").exclude(specialoffers__isnull=False)
-    return render(request, 'index/furniture_bathroom.html', {'products': furniture_bathroom})
+# def furniture_leisure_view(request):
+#     furniture_leisure = Product.objects.filter(category__name="Мебель для отдыха").exclude(specialoffers__isnull=False)
+#     return render(
+#         request,
+#         'index/furniture_leisure.html',
+#         {
+#             'products': furniture_leisure,
+#             'header_class': 'top_catalog__leisure',
+#          }
+#     )
+#
+#
+# def furniture_for_work_view(request):
+#     furniture_for_work = Product.objects.filter(category__name="Мебель для работы").exclude(specialoffers__isnull=False)
+#     return render(
+#         request,
+#         'index/furniture_for_work.html',
+#         {
+#             'products': furniture_for_work,
+#             'header_class': 'top_catalog__for_work',
+#         }
+#     )
+#
+#
+# def furniture_kitchen_view(request):
+#     furniture_kitchen = Product.objects.filter(category__name="Мебель для кухни").exclude(specialoffers__isnull=False)
+#     return render(
+#         request,
+#         'index/furniture_kitchen.html',
+#         {
+#             'products': furniture_kitchen,
+#             'header_class': 'top_catalog__kitchen',
+#         }
+#     )
+#
+#
+# def furniture_child_room_view(request):
+#     furniture_child_room = (
+#         Product.objects.filter(category__name="Мебель для детской").exclude(specialoffers__isnull=False)
+#     )
+#     return render(
+#         request,
+#         'index/furniture_child_room.html',
+#         {
+#             'products': furniture_child_room,
+#             'header_class': 'top_catalog__child_room',
+#         }
+#     )
+#
+#
+# def furniture_bathroom_view(request):
+#     furniture_bathroom = Product.objects.filter(category__name="Мебель для ванной").exclude(specialoffers__isnull=False)
+#     return render(
+#         request,
+#         'index/furniture_bathroom.html',
+#         {
+#             'products': furniture_bathroom,
+#             'header_class': 'top_catalog__bathroom',
+#         }
+#     )
+
+
+def products_by_category_view(request, category_name, page_title, header_class):
+    products_by_category = Product.objects.filter(category__name=category_name).exclude(specialoffers__isnull=False)
+    return render(
+        request,
+        'index/products_by_category.html',
+        {
+            'products': products_by_category,
+            'page_title': page_title,
+            'header_class': header_class,
+        }
+    )
